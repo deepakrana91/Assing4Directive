f28b0978378fb005d4822af0b629be5fe83e2c78
= <=>
2017-07-19 17:14:25 +0530

    STyle Directive [ngStyle] usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Directives';
+  isVisible=false;
+  firstName:string;
+  title1:string;
+  lastName:string;
+  allStudents:string[] = [];
+  
+  showForm() {
+    this.isVisible=true;
+  }
+
+  hideForm() {
+    this.isVisible=false;
+  }
+
+  addTitle() {
+
+    this.title1 = this.firstName+' '+this.lastName;
+    this.isVisible=true;
+  }
+
+
+  
+  addStudent() {
+
+    // this indexof fuction will prevent our array to get any duplicate name 
+    
+    if (this.allStudents.indexOf(this.firstName) == -1) {
+    this.allStudents.push(this.firstName);
+     }
+    
+
+  }   
 }
